abe97b88df591b15c7d5f069ed22c47a0778d228
afg26 <a.f.ghaznawi1991@gmail.com>
2023-07-19 19:26:05 -0400

    asdas

--- PIZZA_WEB_APPLICATIONp/PIZZA_WEB_APPLICATIONp/settings.py
+++ PIZZA_WEB_APPLICATIONp/PIZZA_WEB_APPLICATIONp/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "crispy_forms",
-    "crispy_bootstrap5",
+    #"crispy_bootstrap5",
     'user_login.apps.UserLoginConfig',
     #'Create_your_own.apps.CreateYourOwnConfig',
 
